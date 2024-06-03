5db16f98e722af8d423f28e2167bd609040687c0
benjovitz <dbenjovitz@gmail.com>
2024-06-03 17:05:34 +0200

    story for name header

---
+++ src/components/name-header/name-header.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import type { NameHeaderProps } from './name-header';
+
+import NameHeader from './name-header';
+
+const meta: Meta<NameHeaderProps> = {
+	title: 'Components/NameHeader',
+	component: NameHeader,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof NameHeader>;
+
+export const ChineseName: Story = {
+	args: {
+		name: '青眼白龍', color: '#FF0',
+	},
+};
+
+export const JapaneseName: Story = {
+	args: {
+		name: 'ブルーアイズ・ホワイト・ドラゴン', color: '#BC002D',
+	},
+};
+
+export const AmericanName: Story = {
+	args: {
+		name: 'Tom', color: '#B31942',
+	},
+};
